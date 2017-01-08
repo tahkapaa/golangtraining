ff0c7949b8df297014403e2f1ad3afc958a72502
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-01-08 12:17:13 +0200

    Give slice as an argument to the variadic functions

--- functions/variadic/main.go
+++ functions/variadic/main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Average: ", average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println("Average: ", average(data...))
 }
 
 func average(number ...int) int {
