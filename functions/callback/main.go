edbdc2a59f86ea57b97c26200c0f441982397151
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-01-08 13:19:53 +0200

    Added filter callback

--- functions/callback/main.go
+++ functions/callback/main.go
@@ -8,10 +8,26 @@ func main() {
 		fmt.Println("Number: ", number)
 	}
 	visit(numSlice, printer)
+
+	filterFunc := func(n int) bool {
+		return n > 20
+	}
+
+	fmt.Println(filter(numSlice, filterFunc))
 }
 
 func visit(numbers []int, callback func(int)) {
 	for _, value := range numbers {
 		callback(value)
 	}
+}
+
+func filter(numbers []int, filterFunc func(int) bool) []int {
+	var filteredSlice []int
+	for _, value := range numbers {
+		if filterFunc(value) {
+			filteredSlice = append(filteredSlice, value)
+		}
+	}
+	return filteredSlice
 }
