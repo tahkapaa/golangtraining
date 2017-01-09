02f55f2ed0c1342204a324b3e29a59c89b5c12dd
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-01-09 19:06:39 +0200

    Added slices

---
+++ data_structures/slices/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	myArray := [6]int{1, 2, 3, 4, 5}
+	fmt.Printf("%T\n", myArray)
+	fmt.Println(myArray)
+	fmt.Println(cap(myArray))
+
+	mySlice := []int{1, 2, 3, 4, 5}
+	fmt.Printf("%T\n", mySlice)
+	fmt.Println(mySlice)
+	fmt.Println(cap(mySlice))
+
+	fmt.Println(cap(mySlice[1:2]))
+
+	myString := "Tähkäpää" //[84 195 164 104 107 195 164 112 195 164 195 164]
+	fmt.Println(myString)
+	fmt.Println([]byte(myString))
+	fmt.Println(myString[1]) // 195
+	fmt.Println(myString[2]) // 164 -> ä = [195 164]
+
+	myBytes := []byte{84, 195, 164, 104, 107, 195, 164, 112, 195, 164, 195, 164}
+	fmt.Println(string(myBytes))
+
+	ints := make([][]int, 0)
+	for i := 0; i < 4; i++ {
+		a := make([]int, 0)
+		for j := 0; j < 4; j++ {
+			a = append(a, j)
+		}
+		ints = append(ints, a)
+	}
+	fmt.Println(ints)
+}
