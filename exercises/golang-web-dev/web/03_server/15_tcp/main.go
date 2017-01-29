e8ccc46d50073377826d464e9fc673dee19899e6
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-01-29 18:38:40 +0200

    Exercise 15 - mux over tcp

--- exercises/golang-web-dev/web/03_server/15_tcp/main.go
+++ exercises/golang-web-dev/web/03_server/15_tcp/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -29,24 +30,97 @@ func serve(conn net.Conn) {
 	defer conn.Close()
 	scanner := bufio.NewScanner(conn)
 	i := 0
-	var body string
+	var method, uri string
 	for scanner.Scan() {
 		ln := scanner.Text()
+		if i == 0 {
+			parts := strings.Fields(ln)
+			if len(parts) > 1 {
+				method = parts[0]
+				uri = parts[1]
+			}
+		}
 		if ln == "" {
-			body = fmt.Sprint("<!DOCTYPE html><html><head></head><body><strong>HOLY COW THIS IS LOW LEVEL</strong></body></html>")
-			writeStatus(conn)
-			writeHeader(conn, len(body))
-			io.WriteString(conn, body)
+
 			break
 		}
 		i++
 	}
-	fmt.Println(body)
+	mux(conn, method, uri)
 	fmt.Println("Connection closed")
+}
+
+func mux(w io.Writer, method string, uri string) {
+	fmt.Println("Method, URI", method, uri)
+	switch uri {
+	case "/":
+		index(w, method)
+	case "/apply":
+		apply(w, method)
+	default:
+		notFound(w)
+		writeHeader(w, 0)
+	}
+
+}
+
+func apply(w io.Writer, method string) {
+	switch method {
+	case "GET":
+		getApply(w)
+	case "POST":
+		postApply(w)
+	default:
+		invalidMethod(w)
+	}
+}
+
+func index(w io.Writer, method string) {
+	switch method {
+	case "GET":
+		getIndex(w)
+	default:
+		invalidMethod(w)
+	}
+}
+
+func getIndex(w io.Writer) {
+	body := fmt.Sprint("<!DOCTYPE html><html><head></head><body><strong>HOLY COW THIS IS LOW LEVEL</strong></body></html>")
+	writeResponse(w, body)
+}
+
+func getApply(w io.Writer) {
+	body := fmt.Sprint("<!DOCTYPE html><html><head></head><body><strong>GET APPLY</strong></body></html>")
+	writeResponse(w, body)
+}
+
+func postApply(w io.Writer) {
+	body := fmt.Sprint("<!DOCTYPE html><html><head></head><body><strong>POST APPLY</strong></body></html>")
+	writeResponse(w, body)
+}
+
+func writeResponse(w io.Writer, body string) {
+	writeStatus(w)
+	writeHeader(w, len(body))
+	io.WriteString(w, body)
 }
 
 func writeStatus(w io.Writer) {
 	fmt.Fprint(w, "HTTP/1.1 200 OK\r\n")
+}
+
+func notFound(w io.Writer) {
+	fmt.Fprint(w, "HTTP/1.1 404 NOT FOUND\r\n")
+	body := "404 NOT FOUND"
+	writeHeader(w, len(body))
+	writeResponse(w, body)
+}
+
+func invalidMethod(w io.Writer) {
+	fmt.Fprint(w, "HTTP/1.1 405 INVALID METHOD\r\n")
+	body := "405 INVALID METHOD"
+	writeHeader(w, len(body))
+	writeResponse(w, body)
 }
 
 func writeHeader(w io.Writer, bodyLen int) {
