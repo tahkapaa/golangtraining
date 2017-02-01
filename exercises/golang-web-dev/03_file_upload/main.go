0aa6a42b8eabf55ae1f3ebf8cd0b8a82bf8bdceb
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2017-02-01 21:19:22 +0200

    Testing file upload

---
+++ exercises/golang-web-dev/03_file_upload/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+type person struct {
+	FirstName  string
+	LastName   string
+	Subscribed bool
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+
+	var s string
+	fmt.Println(req.Method)
+	bs := make([]byte, req.ContentLength)
+	req.Body.Read(bs)
+	body := string(bs)
+
+	err := tpl.ExecuteTemplate(w, "index.gohtml", body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Fatalln(err)
+	}
+	io.WriteString(w, s)
+
+}
